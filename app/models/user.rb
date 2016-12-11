37d4266d09533bcf8e815a17253b2b2fc65d21b9
emplumb <emplumb@gmail.com>
2016-12-11 15:17:53 -0600

    Adds bcrypt and faker gems; adds missing associations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :portfolios
+  has_many :investments, through: :portfolios
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+20.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+
+  User.create(
+    name: "#{first_name} #{last_name}",
+    email: "#{first_name}.#{last_name}@gmail.com",
+    username: "#{first_name} + rand(1..90)",
+    password: "password"
+    )
+end
