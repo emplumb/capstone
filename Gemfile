53732951f8842b8d6e10b32a7468acffc45f7dc5
emplumb <emplumb@gmail.com>
2016-12-14 21:16:01 -0600

    Adds fake data to seeds file for all models and adds unirest gem

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ gem 'bcrypt', '~> 3.1.7'
 
 gem 'faker'
 
+gem 'unirest'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,39 @@
-20.times do
+User.destroy_all
+Portfolio.destroy_all
+Investment.destroy_all
+InvestmentPortfolio.destroy_all
+
+investment = Investment.create([
+  {ticker: "TSLA", name: "Tesla", price: 198.69},
+  {ticker: "AAPL", name: "Apple", price: 115},
+  {ticker: "F", name: "Ford", price: 185},
+  {ticker: "GE", name: "General Electric", price: 31.50},
+  {ticker: "CMG", name: "Chipotle", price: 375.70},
+  {ticker: "BABA", name: "Alibaba", price: 91.2},
+  {ticker: "JPM", name: "JP Morgan", price: 84.73},
+  {ticker: "NFLX", name: "Netflix", price: 123.44}
+])
+
+3.times do
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name
 
-  User.create(
+  user = User.new(
     name: "#{first_name} #{last_name}",
-    email: "#{first_name}.#{last_name}@gmail.com",
-    username: "#{first_name} + rand(1..90)",
+    email: "#{first_name.downcase}.#{last_name.downcase}@gmail.com",
+    username: "#{first_name}" + rand(1..99).to_s,
     password: "password"
     )
+  user.save
+
+  portfolio = Portfolio.create(user_id: user.id, name: user.username + " " + "Portfolio")
+
+  InvestmentPortfolio.create([
+    {investment_id: investment.first.id, portfolio_id: portfolio.id},
+    {investment_id: investment.first.id + 2, portfolio_id: portfolio.id},
+    {investment_id: investment.first.id + 4, portfolio_id: portfolio.id}
+  ])
 end
+
+
+
