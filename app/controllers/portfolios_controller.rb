79c8ee3bbba8413372d5e57f50c1d18cd0d8117a
emplumb <emplumb@gmail.com>
2017-01-04 15:49:58 -0600

    Adds several attributes to portfolios show page

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,19 @@
 class PortfoliosController < ApplicationController
+
+  def show
+    @portfolio = Portfolio.find(params[:id])
+    render "show.html.erb"
+
+    ticker = params[:ticker] || "AAPL"
+
+    @barchart = Unirest.get("http://marketdata.websol.barchart.com/getQuote.json?key=a6ff075b20922ed334cf367cab045322&symbols=#{ticker}").body
+    @current_day = @barchart["results"][0]
+
+    @name = @current_day["name"]
+    @symbol = @current_day["symbol"]
+    @current_price = @current_day["lastPrice"]
+    @close_price = @current_day["close"]
+    @percent_change = @current_day["percentChange"]
+  end
+
 end
