9d7ed4cd4e6eee7b0d1d1422741b936c7f469a0e
emplumb <emplumb@gmail.com>
2017-01-04 19:28:45 -0600

    Fixes portfolio error of stocks not pulling from API

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,17 +2,17 @@ class PortfoliosController < ApplicationController
 
   def show
     @portfolio = Portfolio.find(params[:id])
-    render "show.html.erb"
 
     ticker = params[:ticker] || "AAPL"
 
     @barchart = Unirest.get("http://marketdata.websol.barchart.com/getQuote.json?key=a6ff075b20922ed334cf367cab045322&symbols=#{ticker}").body
-    @current_day = @barchart["results"][0]
 
+    @current_day = @barchart["results"][0]
     @name = @current_day["name"]
     @symbol = @current_day["symbol"]
+    @date = @current_day["tradeTimestamp"]
     @current_price = @current_day["lastPrice"]
-    @close_price = @current_day["close"]
+    @open_price = @current_day["open"]
     @percent_change = @current_day["percentChange"]
   end
 
