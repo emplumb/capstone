db8f9ec3ec02f84478bc0a6f604cbf94cdd2eb08
emplumb <emplumb@gmail.com>
2017-01-05 14:21:17 -0600

    Removes User show and index pages and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,20 @@
 Rails.application.routes.draw do
 
   get "/" => "pages#index"
-  get "/users" => "pages#index"
 
   get "/investments" => "investments#index"
   get "/investments/:id" => "investments#show"
 
   get "/portfolios/:id" => "portfolios#show"
 
-  get "/users/:id" => "users#show"
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
 
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  get "/logout" => "sessions#destroy"
+
+  post "/investment_portfolios/" => "investment_portfolios#create"
+
 
 end
