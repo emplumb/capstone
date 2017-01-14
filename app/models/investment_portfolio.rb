ad0b736ab12266da08252613b6a4201662605a6b
emplumb <emplumb@gmail.com>
2017-01-13 22:56:05 -0600

    Creates current price and value methods in IP model
    Also modifies gain_loss and inception_return

--- app/models/investment_portfolio.rb
+++ app/models/investment_portfolio.rb
@@ -2,12 +2,24 @@ class InvestmentPortfolio < ApplicationRecord
   belongs_to :investment
   belongs_to :portfolio
 
+  def quandl
+    Unirest.get("https://www.quandl.com/api/v3/datasets/WIKI/#{ticker}.json?start_date=2017-01-12&api_key=AVB8P1K72xSZsU2SyFZN").body
+  end
+
+  def current_price
+    quandl["dataset"]["data"][0][4]
+  end
+
+  def current_value
+    current_price * shares
+  end
+
   def gain_loss
-    (shares*32) - (shares*purchase_price)
+    current_value - cost_basis
   end
 
   def inception_return
-    gain_loss / (shares*purchase_price)
+    gain_loss / current_value
   end
 
 end
