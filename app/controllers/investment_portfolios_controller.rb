6ff84c8d6f0ae448797ec1940ec820c623b93e2a
emplumb <emplumb@gmail.com>
2017-01-15 16:22:36 -0600

    Allows custom API calls based user input date

--- app/controllers/investment_portfolios_controller.rb
+++ app/controllers/investment_portfolios_controller.rb
@@ -3,8 +3,6 @@ class InvestmentPortfoliosController < ApplicationController
   def index
     @investment_portfolios = InvestmentPortfolio.where(portfolio_id: current_user.portfolios.first.id)
 
-    # @total_portfolio_value = @investment_portfolios.sum(&:current_value)
-
     if @investment_portfolios.length > 0
       render "index.html.erb"
     else
@@ -21,13 +19,25 @@ class InvestmentPortfoliosController < ApplicationController
       redirect_to "/portfolios/#{current_user.portfolios.first.id}"
       flash[:danger] = "Your portfolio already holds this security"
     else
-      share_amount = params[:cost_basis].to_f / params[:purchase_price].to_f
+      # input_date = params[:purchase_date]
+
+      input_date_call = Unirest.get("https://www.quandl.com/api/v3/datasets/YAHOO/#{params[:ticker]}.json?start_date=#{params[:purchase_date]}&end_date=#{params[:purchase_date]}&api_key=AVB8P1K72xSZsU2SyFZN").body
+
+      input_date_price = input_date_call["dataset"]["data"][0][6]
+
+      # def current_value
+      #   current_price * shares
+      # end
+
+      share_amount = params[:cost_basis].to_f / input_date_price.to_f
+      # share_amount = params[:cost_basis].to_f / params[:purchase_price].to_f
       investment_portfolio = InvestmentPortfolio.new(
         investment_id: @investment.id,
         portfolio_id: current_user.portfolios.first.id,
         cost_basis: params[:cost_basis],
         ticker: params[:ticker],
-        purchase_price: params[:purchase_price],
+        # purchase_price: params[:purchase_price],
+        purchase_price: @input_date_price,
         purchase_date: params[:purchase_date],
         shares: share_amount
       )
