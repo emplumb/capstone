fbeeb9ddb5831e0c5147c3860eaa208171831d3d
emplumb <emplumb@gmail.com>
2017-01-22 09:05:08 -0600

    Fixes calling error

--- app/controllers/api/v1/stockprices_controller.rb
+++ app/controllers/api/v1/stockprices_controller.rb
@@ -4,8 +4,23 @@ class Api::V1::StockpricesController < ApplicationController
     @investment = Investment.find(params[:id])
     current_date = 1.day.ago.strftime("%Y-%m-%d")
 
-    api_call = Unirest.get("https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?date.gte=2000-01-01&date.lte=#{current_date}&ticker=#{@investment.ticker}&qopts.columns=date,adj_close&api_key=AVB8P1K72xSZsU2SyFZN").body
-    @daily_prices = api_call["datatable"]["data"]
+    Unirest.timeout(5)
+    5.times do
+      api_call = Unirest.get("https://www.quandl.com/api/v3/datasets/YAHOO/#{@investment.ticker}/data.json?start_date=2006-01-01&column_index=6&order=asc&api_key=AVB8P1K72xSZsU2SyFZN").body
+      if api_call["dataset_data"]
+        @daily_prices = api_call["dataset_data"]["data"]
+        render 'show.json.jbuilder'
+        return
+      else
+        puts "*" * 40
+        puts "problem with #{@investment.ticker}, retrying..."
+        puts "*" * 40
+      end
+    end
+
+
+    # api_call = Unirest.get("https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?date.gte=2000-01-01&date.lte=#{current_date}&ticker=#{@investment.ticker}&qopts.columns=date,adj_close&api_key=AVB8P1K72xSZsU2SyFZN").body
+    # @daily_prices = api_call["datatable"]["data"]
 
     render "show.json.jbuilder"
   end
